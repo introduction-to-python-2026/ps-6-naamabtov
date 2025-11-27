2dd8a504f94970e56fdf5c4c711108ecbab55899
naamabtov <naamabtov@gmail.com>
2025-11-27 14:37:22 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,17 @@
-def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+def create_codon_dict(file):
+  
+    file=open(path)
+    rows=file.readlines()
+    file.close()
+    
+    codon2aa={}
+    for i in rows:
+         newlist=i.strip().split('\t')
+         key= newlist[0]
+         value= newlist[2]
+         codon2aa[key]=value
+  
+    return codon2aa
 
+print(codon2aa)
 
